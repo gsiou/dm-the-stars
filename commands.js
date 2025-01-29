9467c26b26f592c12b3f077cc1b1cb6dbf4f4cbb
Georgios Anastasiou <gsiou@gunet.gr>
2025-01-29 22:02:08 +0200

    commands cleanup

--- commands.js
+++ commands.js
@@ -3,41 +3,6 @@ require('dotenv').config()
 
 const url = `https://discord.com/api/v10/applications/${process.env.APPLICATION_ID}/commands`;
 
-// This is an example CHAT_INPUT or Slash Command, with a type of 1
-// const data = {
-//   name: "blep",
-//   type: 1,
-//   description: "Send a random adorable animal photo",
-//   options: [
-//     {
-//       name: "animal",
-//       description: "The type of animal",
-//       type: 3,
-//       required: true,
-//       choices: [
-//         {
-//           name: "Dog",
-//           value: "animal_dog"
-//         },
-//         {
-//           name: "Cat",
-//           value: "animal_cat"
-//         },
-//         {
-//           name: "Penguin",
-//           value: "animal_penguin"
-//         }
-//       ]
-//     },
-//     {
-//       name: "only_smol",
-//       description: "Whether to show only baby animals",
-//       type: 5,
-//       required: false
-//     }
-//   ]
-// };
-
 const cmds = [{
   name: 'askthestars',
   type: 1,
