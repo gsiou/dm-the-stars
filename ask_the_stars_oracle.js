19b9442ead0bc562effc5c1daec971a161d21b81
Georgios Anastasiou <gsiou@gunet.gr>
2025-01-29 17:42:45 +0200

    ask the stars oracle

---
+++ ask_the_stars_oracle.js
@@ -0,0 +1,105 @@
+/* 
+    The text of this file is by Chris McDowall and is excluded from the LICENSE.
+    See https://www.bastionland.com/2020/12/ask-stars-minimal-solo-rpg.html
+*/ 
+module.exports.symbol = [
+    {
+        symbolic: "The Fang",
+        literal: "hostility / fear"
+    },
+    {
+        symbolic: "The Wings",
+        literal: "freedom / nature"
+    },
+    {
+        symbolic: "The Cage",
+        literal: "protection / obligation"
+    },
+    {
+        symbolic: "The Hand",
+        literal: "creation / misdirection"
+    },
+    {
+        symbolic: "The Mask",
+        literal: "persuasion / shame"
+    },
+    {
+        symbolic: "The Eye",
+        literal: "judgement / secrets"
+    },
+    {
+        symbolic: "The Child",
+        literal: "learning / greed"
+    },
+    {
+        symbolic: "The Traveller",
+        literal: "wandering / chance"
+    },
+    {
+        symbolic: "The Elder",
+        literal: "authority / tradition"
+    },
+    {
+        symbolic: "The Fleet",
+        literal: "direction / struggle"
+    },
+    {
+        symbolic: "The Council",
+        literal: "opposition / cycles"
+    },
+    {
+        symbolic: "The Legion",
+        literal: "unification / identity"
+    }
+];
+
+module.exports.position = [
+    {
+        symbolic: "Rising",
+        literal: "growth / possibility"
+    },
+    {
+        symbolic: "Entombed",
+        literal: "memory / death"
+    },
+    {
+        symbolic: "Twinned",
+        literal: "intimacy / dependency"
+    },
+    {
+        symbolic: "Waning",
+        literal: "hunger / decay"
+    },
+    {
+        symbolic: "Rooted",
+        literal: "stability / plenty"
+    },
+    {
+        symbolic: "Bowed",
+        literal: "submission / mercy"
+    },
+    {
+        symbolic: "Colliding",
+        literal: "change / violence"
+    },
+    {
+        symbolic: "Burning",
+        literal: "honesty / pride"
+    },
+    {
+        symbolic: "Veiled",
+        literal: "faith / deceit"
+    },
+    {
+        symbolic: "Exiled",
+        literal: "guilt / autonomy"
+    },
+    {
+        symbolic: "Crowned",
+        literal: "ambition / ruin"
+    },
+    {
+        symbolic: "Reflected",
+        literal: "reversal / vanity"
+    }
+];
